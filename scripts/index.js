88745376e40beb68ebbb819e0748eeec46387067
Roman Gorev <skier.909@yandex.ru>
2024-07-30 10:31:19 +0300

    fix bags: pull request 2

--- scripts/index.js
+++ scripts/index.js
@@ -3,28 +3,34 @@ const cardContainer = document.querySelector(".places__list");
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
-function addCards(item) {
+function addCards(item, remove) {
   const cardTemplate = document.querySelector("#card-template").content;
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   cardElement.querySelector(".card__title").textContent = item.name;
-  cardElement.querySelector(".card__image").src = item.link;
-  cardElement.querySelector(".card__image").alt = item.name;
+  const cardImage = cardElement.querySelector(".card__image");
+  cardImage.src = item.link;
+  cardImage.alt = item.name;
 
   const removeButton = cardElement.querySelector(".card__delete-button");
-  removeButton.addEventListener("click", removeCard);
-
-  return cardContainer.append(cardElement);
+  
+  function remove () {
+    removeButton.addEventListener("click", removeCard);
+  }
+  remove();
+  
+  return cardElement;
 }
 
 // @todo: Вывести карточки на страницу
 function cards() {
-  initialCards.forEach(addCards);
+  initialCards.forEach(function (elem) {
+    cardContainer.append(addCards(elem));
+  });
 }
 
 cards();
 
 // @todo: Функция удаления карточки
-function removeCard() {
-  const revDiv = this.parentElement;
-  revDiv.remove();
+function removeCard(event) {
+  event.target.closest(".card").remove();
 }
